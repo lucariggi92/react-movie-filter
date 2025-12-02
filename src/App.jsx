cb48f16e637c07fd72653cf7f55b81804aa33f5b
lucariggi92 <riggiluca92@gmail.com>
2025-12-02 17:15:43 +0100

    feat: on change completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,70 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
+
+const films = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+]
+
+function FilmList() {
+  return (
+
+    <div>
+      <ul>
+        {films.map((Curfilm, index) => (
+          <li key={index}>{Curfilm.title}</li>
+        ))}
+      </ul>
+    </div>
+
+  )
+
+}
+
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [filmFiltred, setFilmFiltred] = useState ("")
 
   return (
     <>
-      ciao luca è ora di mettersi a lavoro
+      <div className='container p-5'>
+        <h1>La Nostra Lista Film </h1>
+
+
+        <section>
+
+
+          <input
+            type="text"
+            value={filmFiltred}
+            onChange={(event) => 
+              setFilmFiltred(event.target.value)
+            } />
+        </section>
+
+        <section>
+          <FilmList />
+        </section>
+
+     
+
+    </div >
+
+
+
+
+    
     </>
+
+
   )
 }
 
